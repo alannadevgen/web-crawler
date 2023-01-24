ab4e687e46df21df5d1d245d53d167b875ffd6bb
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-01-24 10:15:17 +0100

    Add crawler folder

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -15,11 +15,12 @@ logging.basicConfig(
 
 class Crawler:
 
-    def __init__(self, urls=[], max_url=os.environ.get("MAX_URL")):
+    def __init__(self, urls=[], max_url=os.environ.get("MAX_URL"), wait_time):
         self.__visited_urls = []
         self.__allowed_urls = []
         self.__urls_to_visit = urls
         self.__MAX_URL = int(max_url)
+        self.wait_time = wait_time
     
     def get_visited_urls(self):
         return self.__visited_urls
@@ -41,7 +42,15 @@ class Crawler:
                 path = urljoin(url, path)
             yield path
 
-    def is_crawlable(self, url):
+    @staticmethod
+    def is_crawlable(url):
+        '''
+        Check if a url can be crawled or not
+        
+        Returns
+        -------
+        bool : True if the url can be crawled, False if not.
+        '''
         rp = RobotFileParser()
         scheme = urlparse(url).scheme
         domain = urlparse(url).netloc
@@ -49,6 +58,11 @@ class Crawler:
         rp.set_url(link)
         rp.read()
         return rp.can_fetch("*", url)
+    
+    @staticmethod
+    def is_valid_url(url):
+        # scheme = urlparse(url).scheme
+        return url.startswith(("https", "http"))
 
     def add_allowed_urls(self, url):
         if url not in self.__allowed_urls:
@@ -63,18 +77,22 @@ class Crawler:
         for url in self.get_linked_urls(url, html):
             self.add_url_to_visit(url)
             
-        logging.info(f'Waiting: {wait_time} seconds')
+        logging.info(f'Waiting {wait_time} seconds')
         sleep(wait_time)
-        self.add_allowed_urls(url=url)
+        is_valid = self.is_valid_url(url)
+        if is_valid:
+            self.add_allowed_urls(url=url)
 
     def run(self):
         while self.__urls_to_visit and len(self.__allowed_urls) < self.__MAX_URL:
             url = self.__urls_to_visit.pop(0)
-            logging.info(f'Crawling: {url}')
+            logging.info(f'Crawling {url}')
             try:
                 is_crawlable = self.is_crawlable(url)
                 if is_crawlable:
                     self.crawl(url, wait_time=2)
+                else:
+                    logging.warning(f'URL {url} could not be crawled')
             except Exception:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
@@ -85,14 +103,3 @@ class Crawler:
         print(len(self.__visited_urls), ' links visited.')
         print(len(self.__allowed_urls), ' links crawled.')
         sleep(5)
-
-if __name__ == '__main__':
-    URL = os.environ.get("URL")
-    crawler = Crawler(urls=[URL], max_url=5)
-    crawler.run()
-    print()
-    print(crawler.get_visited_urls())
-    print()
-    print(crawler.get_allowed_urls())
-    # print()
-    # print(crawler.get_urls_to_visit())
